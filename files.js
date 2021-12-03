289f0c06c4570799270daeae65cff112a7afeeef
taiwohusayn5@gmail.com <alhassaansahih@gmail.com>
2021-12-03 17:19:38 +0100

    Nodejs basics

--- files.js
+++ files.js
@@ -10,15 +10,21 @@ const fs = require('fs');
 
 
 // writing files
-fs.writeFile('./docs/blog1.txt', "hello, world", () => {
-    console.log('flie was written');
-});
+// fs.writeFile('./docs/blog1.txt', "hello, world", () => {
+//     console.log('flie was written');
+// });
 
-fs.writeFile('./docs/blog2.txt', "hello, Ninja", () => {
-    console.log('flie was written');
-})
+// fs.writeFile('./docs/blog2.txt', "hello, Ninja", () => {
+//     console.log('flie was written');
+// })
 
 // directories
+fs.mkdir('./assets', (err) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log('folder created')
 
+})
 
 // reading files
