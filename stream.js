6383d8be0c04802430ef8d147f39c8a8f85df14c
taiwohusayn5@gmail.com <alhassaansahih@gmail.com>
2021-12-04 18:58:47 +0100

    Nodejs basics

--- stream.js
+++ stream.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt');
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 readStream.on('data', (chunck) => {
     console.log("-------New CHUNK --------")
-    console.log(chunck)
+    console.log(chunck);
+    writeStream.write('\nNEW CHUNK\n')
+    writeStream.write(chunck)
 });
