82505c40a7e0a6bb161be0ee6106aedfdf6d1871
taiwohusayn5@gmail.com <alhassaansahih@gmail.com>
2021-12-04 18:54:55 +0100

    Nodejs basics

--- stream.js
+++ stream.js
@@ -3,5 +3,6 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./docs/blog3.txt');
 
 readStream.on('data', (chunck) => {
-
+    console.log("-------New CHUNK --------")
+    console.log(chunck)
 });
