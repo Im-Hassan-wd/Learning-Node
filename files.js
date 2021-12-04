aca785d09a9a43fd62f3a2cffcfeb35c24139416
taiwohusayn5@gmail.com <alhassaansahih@gmail.com>
2021-12-04 18:47:19 +0100

    Nodejs basics

--- files.js
+++ files.js
@@ -36,4 +36,12 @@ if (!fs.existsSync('./assets')) {
 }
 
 
-// reading files
+// deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', err => {
+        if (err) {
+            console.log(err) 
+        }
+        console.log("file deleted");
+    })
+}
