a9452617737cd2f1d43da0a41b12659fb34335c7
taiwohusayn5@gmail.com <alhassaansahih@gmail.com>
2021-12-05 19:38:24 +0100

    client-server

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req, res);
 });
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000');
+})
