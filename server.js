2437a1ca4e0c25977696268a1a09167af0822850
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-12-07 21:40:48 +0100

    request-response

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
-
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
