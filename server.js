ab8648cd44eba6eb92d2ffda226bde7944d7da7a
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-12-07 21:39:01 +0100

    request-response

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     //set header content type
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
 
-    res.write('<p>hello, ninja</p>');
-    res.write('<p>hello again, ninjas</p>');
-    res.end();
+
 });
 
 server.listen(3000, 'localhost', () => {
