074cf76d193d1e5c7171acaffbe63ce5e424c116
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-21 03:50:04 +0100

    request-response

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    let path = './views';
+    let path = './views/';
     switch(req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
