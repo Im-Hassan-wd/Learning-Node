5028b1064b4fb5eb9feafcb6883776f158d2c172
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-25 14:09:09 +0100

    view-engines

--- app.js
+++ app.js
@@ -10,21 +10,18 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname });
   res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  //res.sendFile('./views/about.html', { root: __dirname });
   res.render('about');
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render();
+  res.render('create');
 })
 
 // 404
 app.use((req, res) => {
-  //res.status(404).sendFile('./views/404.html', { root: __dirname });
   res.status(404).render('404');
 });
