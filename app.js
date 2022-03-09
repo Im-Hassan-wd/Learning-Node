50c800680230b656d9c00c370eeabfb6536fe943
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-09 23:10:08 +0100

    get, post, delete request

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 // routes
