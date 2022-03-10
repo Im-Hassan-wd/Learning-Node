5533bd8b272e0e52afcb8d21ad4cc9f82926bc95
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-10 03:05:53 +0100

    express route MVC

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const Blog = require('../models/blog');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  Blog.find().sort({ createdAt: -1})
-   .then(result => {
-     res.render('index', { title: 'All Blogs', blogs: result})
-   })
-   .catch(err => {
-     console.log(err);
-   })
+  
 });
 
 router.post('/', (req, res) => {
