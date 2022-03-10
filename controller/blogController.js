2d538e33fb99c35621332d28ff1f1f9caceaa757
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-10 03:13:49 +0100

    express route MVC

--- controller/blogController.js
+++ controller/blogController.js
@@ -26,8 +26,34 @@ const blog_create_get = (req, res) => {
     res.render('create', { title: 'create a new blog'});
 }
 
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then(result => {
+        res.redirect('/')
+    })
+    .catch(err =>  {
+        console.log(err);
+    })
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({ redirect: '/'})
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
 module.exports = {
     blog_index,
     blog_details,
-    blog_create_get
+    blog_create_get,
+    blog_create_post,
+    blog_delete
 }
